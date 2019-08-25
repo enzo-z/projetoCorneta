47c9c36881c6b8fdfb5c2dfb6386e60934604775
enzo-z <ezamoradev@gmail.com>
2019-08-25 19:07:07 -0300

    Pagina cadastro.php com o formulario criada

---
+++ cadastro.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Cadastro Conta</title>
+    </head>
+    <body>
+        <form action="POST" id="cadastro">
+            Nome<input type="text" name="">
+            Sobrenome<input type="text" name="">
+            <br>
+            Idade<input type="text" name="">
+            <br>
+            Diga seu sexo:
+            <br>
+            Masculino<input type="radio" name="sexo" value="M">
+            Feminino<input type="radio" name="sexo" value="F">
+            Anônimo<input type="radio" name="sexo" value="O">
+            <br>
+            Login<input type="text" name="Login">
+            <br>
+            Senha<input type="password" name="password">
+
+        
+        </form>
+
+        <!--<script src="js/cadastro.js"></script>!-->
+    </body>
+
+e
+</html>
